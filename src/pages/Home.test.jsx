318aa65b15cdd055e9a75ab677fc42dbc93ffbfa
Fernando Guevara <luisfernandoguevara95@gmai.com>
2023-02-20 16:46:37 -0600

    add Home page test

---
+++ src/pages/Home.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+
+import Home from './Home';
+
+describe('Home', () => {
+  it('renders headline', () => {
+    const{ getByRole }= render(<Home />);
+
+    expect(getByRole('heading', {name: /regim/i})).toBeInTheDocument();
+  });
+});
