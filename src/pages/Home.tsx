fac71008b2b982abec275320cff0c9f460d552d3
Fernando Guevara <luisfernandoguevara95@gmai.com>
2023-02-21 10:33:43 -0600

    intefrate Features component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from "../components/Navbar";
+import { Features } from "../sections/Features";
 
 const Home = () => {
 
   return (
     <>
       <Navbar />
+      <Features />
     </>
   )
 }
