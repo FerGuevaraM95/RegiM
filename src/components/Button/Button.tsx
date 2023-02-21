b7537f2112d6e85746baa3e8ee479b6551993e51
Fernando Guevara <luisfernandoguevara95@gmai.com>
2023-02-20 18:02:17 -0600

    create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    padding: 10px 16px;
+    border:none;
+    border-radius: 6px;
+    background: #0876DD;
+    color: #FFFFFF;
+    cursor: pointer;
+`;
