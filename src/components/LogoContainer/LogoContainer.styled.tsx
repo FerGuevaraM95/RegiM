d92335e6fbb932b41462ff7c9f6537e75b2adf03
Fernando Guevara <luisfernandoguevara95@gmai.com>
2023-02-21 16:44:44 -0600

    create LogoContainer component

---
+++ src/components/LogoContainer/LogoContainer.styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 56px;
+    height: 56px;
+    box-shadow: 4px 12px 24px rgba(28, 44, 64, 0.08);
+    border-radius: 100px;   
+`;
---
+++ src/components/LogoContainer/LogoContainer.tsx
@@ -0,0 +1,30 @@
+import { Container } from './LogoContainer.styled';
+
+import { Logo1 } from '../../components/Icons/Logo1';
+import { Logo2 } from '../../components/Icons/Logo2';
+import { Logo3 } from '../../components/Icons/Logo3';
+import { Logo4 } from '../../components/Icons/Logo4';
+import { Logo5 } from '../../components/Icons/Logo5';
+import { Logo6 } from '../../components/Icons/Logo6';
+import { Logo7 } from '../../components/Icons/Logo7';
+
+export type LogoContainerProps ={
+    name:string;
+}
+
+export const LogoContainer = ({name}: LogoContainerProps) => {
+
+    const logosComponent = {
+        'Logo1': <Logo1 />,
+        'Logo2': <Logo2 />,
+        'Logo3': <Logo3 />,
+        'Logo4': <Logo4 />,
+        'Logo5': <Logo5 />,
+        'Logo6': <Logo6 />,
+        'Logo7': <Logo7 />,
+    }
+
+  return (
+    <Container>{logosComponent[name]}</Container>
+  )
+}
