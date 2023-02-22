fddcb56066e5cd5fe5d7f67e1b7aa688af80ff6d
Fernando Guevara <luisfernandoguevara95@gmai.com>
2023-02-21 21:43:55 -0600

    Add responsive to Integrations setion

--- src/components/LogoContainer/LogoContainer.styled.tsx
+++ src/components/LogoContainer/LogoContainer.styled.tsx
@@ -8,4 +8,9 @@ export const Container = styled.div`
     height: 56px;
     box-shadow: 4px 12px 24px rgba(28, 44, 64, 0.08);
     border-radius: 100px;   
+
+    @media (min-width: 768px) {
+        width: 80px;
+    height: 80px;
+    }
 `;
--- src/sections/Integrations/Integrations.styled.tsx
+++ src/sections/Integrations/Integrations.styled.tsx
@@ -2,8 +2,15 @@ import styled from 'styled-components';
 
 
 export const Section = styled.section`
+    display: flex;
+    flex-direction: column;
     margin: 48px 0 72px;
     text-align: center;
+
+    @media (min-width: 1440px) {
+       flex-direction: row;
+       margin: 70px 90px 220px;
+    }
 `;
 
 export const Title = styled.h2`
@@ -11,13 +18,29 @@ export const Title = styled.h2`
     font-weight: 600;
     line-height: 48px;
     color: #111827;
+
+     @media (min-width: 768px) {
+        font-size: 48px;
+        line-height: 60px;
+    }
+
+    @media (min-width: 1440px) {
+        text-align: left;
+        font-size: 56px;
+        line-height: 72px;
+    }
 `;
 
 export const Subtitle = styled.p`
     margin-bottom: 40px;
     font-size: 16px;
-    color: #374151;
     line-height: 24px;
+    color: #374151;
+
+    @media (min-width: 768px) {
+        font-size: 18px;
+        line-height: 28px;
+    }
 `;
 
 export const Logos = styled.div`
@@ -25,4 +48,9 @@ export const Logos = styled.div`
     justify-content: center;
     flex-wrap: wrap;
     gap: 32px;
+
+    @media (min-width: 768px) {
+        gap: 80px;
+        margin: 0 80px;
+    }
 `;
